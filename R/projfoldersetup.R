639d550b01fc75507444b34826c59c5e9d5764e7
boriellis <aaellis@ucsc.edu>
2023-11-02 15:55:21 -0700

    removed the call to the function

--- R/projfoldersetup.R
+++ R/projfoldersetup.R
@@ -36,7 +36,3 @@ project_setup <- function() {
   writeLines("The scripts that actually do things are stored in the root directory, but if your project has many scripts, you might want to organize them in a directory of their own.", 
              "scripts/README.md")
 }
-
-
-
-project_setup()
